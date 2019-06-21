a8b5124041d202637e9133af7c514044d44493be
Blake <blake.doyle@hotmail.com.au>
2019-06-20 17:08:43 -0700

    update entry if one already exists

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -1,7 +1,15 @@
 const ItemModel = require("./../database/models/item_model");
 
 async function create(req, res) {
+    
     const { name, quantity, category, measurement, protein, carbs, fat, sugar } = req.body;
+
+    const foundItem = await checkItemExists(name)
+    
+    if (foundItem){
+        await addQuantity(foundItem, quantity);
+    }
+
     let { weight, price } = req.body;
 
     const calories = await calculateCalories(protein, carbs, fat);
@@ -12,6 +20,24 @@ async function create(req, res) {
     const item = await ItemModel.create({ name, category, quantity, weight, measurement, price, protein, carbs, fat, sugar, calories});
 
     res.json(item);
+}
+
+async function checkItemExists(name){
+    const item = await ItemModel.findOne({name: name});
+    if (item){
+        console.log("found item");
+        return item;
+    }
+    console.log("item not found");
+    return false;
+}
+
+async function addQuantity(foundItem, quantity){
+    let oldQuantity = Number(foundItem.quantity);
+    let newQuantity = Number(quantity);
+    let newestQuantity = (oldQuantity + newQuantity);
+    const item = await ItemModel.findOneAndUpdate(foundItem._id, {quantity: newestQuantity});
+    return;
 }
 
 async function update(req, res, next) {
