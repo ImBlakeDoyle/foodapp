df7797d917d48a0c51b448e313c1fe2df4788d7e
Blake <blake.doyle@hotmail.com.au>
2019-08-11 21:32:29 -0700

    find each of the ingredients if they exist in db

--- controllers/recipe_controller.js
+++ controllers/recipe_controller.js
@@ -1,11 +1,29 @@
 const RecipeModel = require("../database/models/recipe_model");
+const ItemModel = require("../database/models/item_model");
 
 async function create(req, res) {
-    const {name, ingredients, method, image} = req.body;
+    let {name, ingredients, method, image} = req.body;
+    // console.log(ingredients)
+
+    for (let i in ingredients) {
+        // console.log(ingredients[i]);
+        checkItem(ingredients[i]);
+    }
+
 
     const recipe = await RecipeModel.create({ name, ingredients, method, image});
 
     res.json(recipe);
+}
+
+async function checkItem(ingredient){
+    const item = await ItemModel.findOne({name: ingredient});
+    if (item){
+        console.log("found item");
+        return item;
+    }
+    console.log("item not found");
+    return false;
 }
 
 module.exports = {
