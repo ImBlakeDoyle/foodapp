f25a7aef623cc9f7f6260d7fe2e7bb2b1b50b2cc
Blake <blake.doyle@hotmail.com.au>
2019-08-13 10:09:53 -0700

    add relations between items and recipe

--- controllers/recipe_controller.js
+++ controllers/recipe_controller.js
@@ -3,23 +3,35 @@ const ItemModel = require("../database/models/item_model");
 
 async function create(req, res) {
     let {name, ingredients, method, image} = req.body;
-    // console.log(ingredients)
 
-    for (let i in ingredients) {
-        // console.log(ingredients[i]);
-        checkItem(ingredients[i]);
-    }
-
+    await splitIngredients(ingredients)
+        // .then(res => console.log(`res is ${res}`))
+        .then(res => {
+            ingredients = res;
+            console.log(ingredients);
+        })
+        // .then(console.log(updatedIngredients))
+        .catch(err => console.log(err));
 
     const recipe = await RecipeModel.create({ name, ingredients, method, image});
 
     res.json(recipe);
 }
 
+async function splitIngredients(ingredients){
+    let newIngredients = []
+    for (let i in ingredients) {
+       await checkItem(ingredients[i])
+       .then(res => newIngredients.push(res))
+    //    .then(console.log(`Updated ingredients are ${newIngredients}`))
+       .catch(err => console.log(err));
+    }
+    return newIngredients;
+}
+
 async function checkItem(ingredient){
     const item = await ItemModel.findOne({name: ingredient});
     if (item){
-        console.log("found item");
         return item;
     }
     console.log("item not found");
